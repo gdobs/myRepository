b8caf41d42b962263d64f06dd10d629c7ecc0b8a
Geoff Dobson <gdobson@mail.greenriver.edu>
2018-04-12 19:51:41 -0700

    Added code in hello.php to make it HTML5 valid

--- helloworld2.php
+++ helloworld2.php
@@ -1,9 +1,19 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: Geoff
  * Date: 4/12/2018
  * Time: 7:32 PM
  **/
- echo 'Hello World';
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Hello</title>
+</head>
+<body>
+<?php
+echo '<h1>Hello, world!</h1>';
 ?>
+</body>
+</html>
